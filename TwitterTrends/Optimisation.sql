440f2a968a7fe63fadec602006573da247a51117
Charles Bernard <bernardcharles@gmail.com>
2020-04-14 16:28:49 +0800

    Update: README.md TwitterTrends

--- TwitterTrends/Optimisation.sql
+++ TwitterTrends/Optimisation.sql
@@ -1,9 +1,20 @@
 -- Databricks notebook source
 OPTIMIZE tweets.`bronze`;
 
+-- COMMAND ----------
+
 OPTIMIZE tweets.`siver`;
 
+-- COMMAND ----------
+
 OPTIMIZE tweets.`gold`;
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC spark.sql("set spark.databricks.delta.retentionDurationCheck.enabled=false")
+-- MAGIC 
+-- MAGIC spark.sql("VACUUM tweets.`bronze` RETAIN 0 HOURS")
 
 -- COMMAND ----------
 
